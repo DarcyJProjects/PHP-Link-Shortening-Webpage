6868abcd83acf950c244fa69502d50eae4d9a174
Darcy <contactdarcy@darcyjprojects.xyz>
2022-11-29 12:46:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,24 @@ a {
 <?php
 //error_reporting(0);
 //CONFIG------------------------------------------------
-$shortenedLength = 4;
+$shortenedLength = 5;
 
 //Include "/" at end of url and directory:
 $webserver = "https://link.darcyjprojects.xyz/";
 $webserverDir = "/var/www/link.darcyjprojects.xyz/";
+$linklistFile = "/var/www/link.darcyjprojects.xyz/admin/linklist.txt";
 $password = "PASSWORD";
 //------------------------------------------------------
+
+//Redirect to https (443) if on http (80) protocol
+$protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+if ($protocol == "http"){
+    header('Location: ' . $webserver);
+}
+
 ?>
 
-<h1>Link Shortener<br><a href="https://link.darcyjprojects.xyz/vgTR" target="_blank" style="font-size: 15px;">Github</a></h1>
+<h1>Link Shortener<br><a href="https://github.com/DarcyJProjects/PHP-Link-Shortening-Webpage" target="_blank" style="font-size: 15px;">Github</a></h1>
 
 <form action="" method="post">
 <a>Link: </a><input type="text" name="url" style="width:300px;"><br>
@@ -35,6 +43,23 @@ $password = "PASSWORD";
 
 <?php
 $url = $_POST['url'];
+
+//Add https to url if it doesn't contain http or https
+$illegal = false;
+if (str_contains($url, "https")) {
+    $illegal = false;
+} else {
+    $illegal = true;
+}
+if (str_contains($url, "http")) {
+    $illegal = false;
+} else {
+    $illegal = true;
+}
+if ($illegal == true){
+    $url = "https://" . $url;
+}
+
 
 if ($_POST['password'] == $password){
     if ($url == NULL){
@@ -46,6 +71,12 @@ if ($_POST['password'] == $password){
         $shortenedURL = $webserver . $shortenedName;
         $shortenedFile = $webserverDir . $shortenedName . ".php";
         $fileContent = "<?php\nheader('Location: $url');\nexit;\n?>";
+        $file2Content = $shortenedName . " - " . $url . "\n";
+
+        //Save to link list for log
+        $file2 = fopen($linklistFile, "a") or die("Unable to write to list file!");
+        fwrite($file2, $file2Content);
+        fclose($file2);
 
         //Create file
         $file = fopen($shortenedFile, "w") or die("Unable to create redirect file!");
@@ -53,7 +84,7 @@ if ($_POST['password'] == $password){
         fclose($file);
 
         //Output
-        echo "<a>Success!</a><br>";
+        echo "<font color='green'><a>Success!</a></font><br>";
         echo "<b><a>Shortened URL: </a></b><a href='$shortenedURL'>$shortenedURL</a><br>";
         echo "<b><a>Destination: </a></b><a href='$url'>$url</a><br>";
     }
@@ -74,7 +105,7 @@ if ($_POST['password'] == $password){
 
 //Generate random name with supplied length
 function generateName($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters = '0123456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ'; //no i, I, l, L to remove confusion
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
